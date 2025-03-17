c0d1aab6e5f8a697025445e30d1797f0bbc39fef
Parth-2601 <pmp020601@gmail.com>
2025-03-16 22:42:19 -0400

    Updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,27 +7,44 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>
-         Hello, I'm <br />
+          Hello, I'm <br />
           <TypeAnimation
-            sequence={["PARTHKUMAR", 1000, 
-                        "A FULL-STACK DEVELOPER", 1000, 
-                        "UI/UX DESIGNER", 1000]}
+            sequence={[
+              "PARTHKUMAR",
+              1000,
+              "A FULL-STACK DEVELOPER",
+              1000,
+              "UI/UX DESIGNER",
+              1000,
+            ]}
             wrapper="span"
             speed={50}
             repeat={Infinity}
           />
         </h1>
         <p className={styles.description}>
-          I'm a dedicated full-stack developer with expertise in building scalable and efficient web applications using React, Node.js, and modern development tools. I am passionate about creating seamless user experiences and optimizing backend performance. Feel free to connect with me to discuss potential collaborations or opportunities.
+          I'm a dedicated full-stack developer with expertise in building
+          scalable and efficient web applications using React, Node.js, and
+          modern development tools. I am passionate about creating seamless user
+          experiences and optimizing backend performance. Feel free to connect
+          with me to discuss potential collaborations or opportunities.
         </p>
-        <a
-          href="https://drive.google.com/uc?export=download&id=1qvPluWkPrTei2nDO6eTGXsPb8LDk9yp3"
-          className={styles.downloadBtn}
-          download
-        >
-          <img src={getImageUrl("hero/download.png")} alt="Download resume" />
-          <span>Download Resume</span>
-        </a>
+        <div className={styles.buttonContainer}>
+          <a
+            href="https://drive.google.com/file/d/1qvPluWkPrTei2nDO6eTGXsPb8LDk9yp3/view?usp=drive_link"
+            className={styles.downloadBtn}
+            download
+          >
+            <span>Resume</span>
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1W3AZOZcVpLbugTZSsvusbSWCZlHU_mLU/view?usp=drive_link"
+            className={styles.downloadBtn}
+            download
+          >
+            <span>Cover Letter</span>
+          </a>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
